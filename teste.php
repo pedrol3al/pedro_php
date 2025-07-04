135b231d153d987eebc64c0c8d07df1730036a13
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-07-04 15:39:50 -0300

    Introdução php

---
+++ teste.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro programa PHP</title>
+</head>
+<body>
+
+<?php
+
+    echo "<h1> Hello Word, PHP-8!</h1>";
+
+?>
+
+<?php 
+
+phpinfo();
+
+?>
+    
+</body>
+</html>
